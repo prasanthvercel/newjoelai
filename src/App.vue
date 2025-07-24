9bfbfe30c34fe03c4b54c3ef068d5171eb537123
prasanthvercel <prasanthvercel@gmail.com>
2025-07-24 10:05:46 +0000

    changes to app nav bar top

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <v-app-bar app color="pink-darken-1" elevation="1">
       <v-container class="d-flex align-center">
         <v-avatar size="40" rounded="circle">
-          <v-img src="/logo.png" alt="Logo"></v-img>
+          <v-img src="/logo.jpg" alt="Logo"></v-img>
         </v-avatar>
-        <v-toolbar-title class="ml-3 text-white">Your App Name</v-toolbar-title>
+        <v-toolbar-title class="ml-3 text-white">Joel Ai</v-toolbar-title>
         <v-spacer></v-spacer>
         <!-- Navigation links can go here -->
       </v-container>
